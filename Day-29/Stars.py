045d497dec5df5306669f40fdbed19f1f8a1ead8
Vayanandana Buddavarapu <vayanandanabud@gmail.com>
2026-02-02 21:47:07 +0530

    Day 29: Python basics Problems starts problem

---
+++ Day-29/Stars.py
@@ -0,0 +1,13 @@
+'''"Write a program that reads a word and prints the word in "   word   " format.
+
+Input
+The input will be a single line containing a string.
+
+Output
+The output should be a single line containing a string in the format shown in the sample output.
+
+Explanation
+For example, if the given word is Magician, the output should be    Magician   "'''
+
+word = input()
+print("* * * " + word + " * * *")
