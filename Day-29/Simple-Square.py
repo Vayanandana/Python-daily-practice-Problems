dd6888a0d0e4381f5f876ba37c39aa15664d067c
Vayanandana Buddavarapu <vayanandanabud@gmail.com>
2026-02-02 20:02:33 +0530

    Day 29: Python Problem Simple-Square

---
+++ Day-29/Simple-Square.py
@@ -0,0 +1,4 @@
+"In this coding question, you need to write a program that prints a simple square using the star (*) symbol. "
+"The square should have two stars on the first line and two stars on the second line."
+print("*" * 2)
+print("*" * 2)
