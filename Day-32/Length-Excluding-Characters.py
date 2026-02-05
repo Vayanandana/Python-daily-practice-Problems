3cb16e66c5bf11b14386c6314bec67354c4e01db
Vayanandana Buddavarapu <vayanandanabud@gmail.com>
2026-02-05 22:58:04 +0530

    Day-31 Basic Problem

---
+++ Day-32/Length-Excluding-Characters.py
@@ -0,0 +1,4 @@
+word = input()
+word_length = len(word) - 2
+result = word_length
+print(result)
---
+++ Day-32/Reverse-the-Digits.py
@@ -0,0 +1,5 @@
+two_digit = input()
+first_digit = two_digit[0]
+last_digit = two_digit[1]
+print(last_digit + first_digit )
+#print(two_digit[1] + two_digit[0])
