2bd76b7309b2bc4b402af98ba0deda530e885644
Vayanandana Buddavarapu <vayanandanabud@gmail.com>
2026-02-08 23:38:23 +0530

    Day-35 python basic problems

---
+++ Day-35/Area-of-a-Rectangle.py
@@ -0,0 +1,19 @@
+'''
+Docstring for Day-35.Area-of-a-Rectangle
+Input:
+
+The first line of input contains the length of the rectangle, which is an integer.
+The second line of input contains the breadth of the rectangle, which is an integer.
+
+Output:
+
+Print the Area of Rectangle.
+
+Explanation:
+
+For example, when the length is 4 and breadth is 3, then A = 3 × 4, therefore A = 12.
+'''
+length = int(input())
+breadth = int(input())
+rectangle = length * breadth
+print(rectangle)
---
+++ Day-35/Calculate-the-Perimeter-of-a-Rectangle.py
@@ -0,0 +1,22 @@
+'''
+Docstring for Day-35.Calculate-the-Perimeter-of-a-Rectangle
+Let's learn how to calculate the perimeter of a rectangle using Python step by step.
+
+Step 1: Understand the problem
+We need to write a program that calculates the perimeter of a rectangle. The formula to calculate the perimeter of a rectangle is:
+
+Perimeter = 2 × (Length + Breadth)
+
+We'll get the length and breadth from the user as input, and then calculate the perimeter.
+
+Step 2: Get the length and breadth from the user
+First, let's ask the user to enter the length and breadth of the rectangle. We'll use the input() function to get the values.
+'''
+length_rectangle =input()
+breadth_rectangle =input()
+
+length_rectangle = int(length_rectangle )
+breadth_rectangle = int(breadth_rectangle)
+area = 2 *  (length_rectangle + breadth_rectangle)
+result = area 
+print(result)
