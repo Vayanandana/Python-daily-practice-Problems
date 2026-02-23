ba3ee509dd4c2d94e8f442ee17d86b7561426465
Vayanandana Buddavarapu <vayanandanabud@gmail.com>
2026-02-23 23:12:16 +0530

    python basic problems

---
+++ Day-45/Greater than Nine.py
@@ -0,0 +1,7 @@
+A = int(input())
+B = int(input())
+check_1 = A > 9
+check_2 = B > 9
+condition = check_1 and check_2
+result = condition
+print(result)
---
+++ Day-45/Negative Numbers.py
@@ -0,0 +1,7 @@
+A = int(input())
+B = int(input())
+less_than_A = A < 0
+less_than_B = B < 0
+check = less_than_A and less_than_B
+result = check
+print(result)
